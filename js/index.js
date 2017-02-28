00911442158544b7a38a5b26842f7eb4c6b7596e
Vladyslav Tychyna <tychyna.te@gmail.com>
2017-02-28 17:58:50 +0200

    fix search field

---
+++ js/index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Vladyslav on 28.02.2017.
+ */
+$().ready(function () {
+    $('.big-slider-container').slick({
+        'prevArrow':'<button type="button" class="slick-prev">Previous</button>',
+        'nextArrow':'<button type="button" class="glyphicon glyphicon-arrow-right slick-next ">Next</button>'
+    });
+});
