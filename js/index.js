10680e599e337cb8e58e6155bd5d3653ee41c631
Vladyslav Tychyna <tychyna.te@gmail.com>
2017-03-14 17:21:10 +0200

    all page

--- js/index.js
+++ js/index.js
@@ -10,6 +10,44 @@ $().ready(function () {
 
     $('.products-slider-container').slick({
         slidesToShow: 6,
-        slidesToScroll: 6
+        slidesToScroll: 6,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 4,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+
+
+    var myLatLng = {lat: 50.293371, lng: 28.702225};
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center:myLatLng,
+        zoom: 19
+    });
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: 'А.ТОМ'
     });
 });
